890601ac3556a7b951c8e18f4f4e143fa09b3ab7
F4k1ng1t <156473484+F4k1ng1t@users.noreply.github.com>
2026-05-14 23:53:42 -0700

    initial

---
+++ Project-TecFight/Assets/Scripts/Character/Fighter.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    // will figure this out later, but for now, this is just a placeholder for the character's combat abilities and stats
+}
