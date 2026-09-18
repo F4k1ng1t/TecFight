1f126212a41bab364847c8a53145ff694732eecd
CollinWilkerson <wilkerc2@my.erau.edu>
2026-09-18 12:11:33 -0700

    enable spawned hitboxes to collide with hurtboxes
    Slight issue, the player has hutboxes that their hitboxes will collide with.

--- TecFight3D/Assets/Scripts/Hitbox/SpawnHitbox.cs
+++ TecFight3D/Assets/Scripts/Hitbox/SpawnHitbox.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class SpawnHitbox : MonoBehaviour
 {
@@ -7,24 +8,46 @@ public class SpawnHitbox : MonoBehaviour
     [SerializeField] float radius = 0.005f;
     [SerializeField] float height = 0;
     [SerializeField] [Range(0, 360)] float sendAngle = 90;
+    [SerializeField] float hitStrength = 0.1f;
     [SerializeField] int priority = 0;
     [SerializeField] LayerMask targetLayer;
 
-    public void Spawn()
+    private void OnDrawGizmos()
+    {
+#if (UNITY_EDITOR) //only draw debug gizmos in the unity editor
+        Gizmos.DrawSphere(parentBone.position, radius);
+        Gizmos.DrawLine(parentBone.position, parentBone.position + new Vector3(hitStrength * Mathf.Asin(sendAngle), hitStrength * Mathf.Acos(sendAngle)));
+#endif
+    }
+
+    public void StartSpawn()
     {
         //Okay i was probably thinking of this wrong
         //the hitbox isn't a gameobject, its just a check that pops up sometimes
         //using physics.checkcapsule would probably be best
         //use the parent bone to find the top bottom and angle
         // radius translates directly
+        StartCoroutine(Hitbox());
     }
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator Hitbox()
     {
-        if(other.gameObject.layer == targetLayer.value)
+        Debug.Log("Checkpoint 1");
+        float startTime = Time.time;
+        while(activeFrames/60f > Time.time - startTime)
         {
-            //deal damage
-            //need some way to reference the character controller, most obvious is to put a script on each hurtbox that has some reference to the character
+
+            Debug.Log("Checkpoint 2");
+            Collider[] hitHurtboxes = Physics.OverlapCapsule(parentBone.position, parentBone.rotation.eulerAngles.normalized * height, radius, targetLayer, QueryTriggerInteraction.Collide);
+            if (hitHurtboxes.Length > 0)
+            {
+                //we hit a hurtbox
+                Debug.Log("hit " + hitHurtboxes[0].gameObject.name);
+                //hitHurtboxes[0].transform.root.GetComponent<FighterStateMachine>(); //put a bitch in hitstun
+            }
+            yield return (new WaitForEndOfFrame());
         }
     }
+
+
 }
